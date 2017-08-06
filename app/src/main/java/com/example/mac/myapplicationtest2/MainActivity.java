a4e3f7fa74613eefa7b174fa433f0bdcf961eb3f
kk6665555 <49990115@stust.edu.tw>
2017-08-06 10:29:11 +0800

    0806

---
+++ app/src/main/java/com/example/mac/myapplicationtest2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mac.myapplicationtest2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
